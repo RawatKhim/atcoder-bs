3014311c37c751df4a5c34af756d12db89338588
AP19081 ラワット キム　バハズル <ap19081@azec.ac.jp>
2021-02-17 14:54:24 +0900

    087b

--- abc087b.py
+++ abc087b.py
@@ -4,15 +4,13 @@ a = int(input())
 b = int(input())
 c = int(input())
 x = int(input())
-nu_combination = 0
+num_combination = 0
 for n500 in range(0,a+1):
     for n100 in range(0,b+1):
-        for n50 in range(0,c+1):
-            amount = n500*500+n100*100+n50*50
-
-            if amount == x :
-                #print(n500, n100, n50, amount)
-                num_combination += 1
+        n50 = (x - n500*500 - n100*100)// 50
+        if n50 <= c and n50>=0:
+            #print(n500, n100,n50)
+            num_combination += 1
 
 
 print (num_combination)
